cb4d540be1f14f2d90525470c5e242f5ff9ff18e
Joy Lin <vvlnote@hotmail.com>
2019-05-20 22:59:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,5 +18,6 @@ class PostsController < ApplicationController
     @post.title = params[:title]
     @post.description = params[:description]
     @post.save
+    redirect_to posts_path
   end
 end
